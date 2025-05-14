bf424ad3e7fe03c791cd6f6c28b3bdf50ca23120
Dina Rolinza <drolinza@gmail.com>
2025-05-14 12:53:35 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { Route, Router, Routes } from "react-router-dom";
+import "./App.css";
+import { LandingPage } from "./pages/LandingPage";
+
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<LandingPage/>} />
+    </Routes>
+  );
+}
+
+export default App;
---
+++ src/pages/LandingPage.jsx
@@ -0,0 +1,9 @@
+import { Link } from "react-router-dom";
+
+export const LandingPage = () => {
+    return(
+      <div className="">
+        
+      </div>
+    )
+}
